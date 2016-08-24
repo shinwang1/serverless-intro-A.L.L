1a331cc27de123214d2fd4b0b31f4c608f8605d2
Luis De Castro <luisdecastro510@gmail.com>
2016-08-23 22:01:18 -0700

    add directories and basic skeletons

---
+++ part-2/part-2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  part-2
+//
+//  Created by Luis De Castro on 8/23/16.
+//  Copyright © 2016 Lu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
