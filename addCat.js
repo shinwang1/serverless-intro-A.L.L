57fd83a7c92b6f78eb6e4d330c49058b24e117d3
jamesboyd2008 <jamesboyd2008@gmail.com>
2016-09-07 02:43:14 -0700

    Break apart index.html

---
+++ addCat.js
@@ -0,0 +1,25 @@
+
+addCat = (catName, catCount, catBack) => {
+  fetch('https://po4m3psh3d.execute-api.us-east-1.amazonaws.com/prod/CatDBManager', {
+    method: "POST",
+    mode: 'cors',
+    body: "{\"operation\":\"create\",\"tableName\":\"CatTable\",\"payload\":{\"Item\":{\"Id\":\"" + (catCount + 1).toString() + "\",\"name\":\"" + catName + "\"}}}"
+  })
+  .then(
+    (response) => {
+      return response.json()
+    }
+  )
+  .then(
+    (data) => {
+      console.log('addCat worked')
+      $( "#cats" ).empty();
+      return getCats(catBack)
+    }
+  )
+  .catch(
+    (err) => {
+      console.log("'Tis erroneous, Sire: ", err)
+    }
+  )
+};
