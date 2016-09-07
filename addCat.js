5019b6a44266203a1b3e3e35c1c273c0c0aa1b2a
jamesboyd2008 <jamesboyd2008@gmail.com>
2016-09-07 14:28:18 -0700

    Update syntax of body prop of fetch argument.

--- addCat.js
+++ addCat.js
@@ -3,7 +3,18 @@ addCat = (catName, catCount, catBack) => {
   fetch('https://po4m3psh3d.execute-api.us-east-1.amazonaws.com/prod/CatDBManager', {
     method: "POST",
     mode: 'cors',
-    body: "{\"operation\":\"create\",\"tableName\":\"CatTable\",\"payload\":{\"Item\":{\"Id\":\"" + (catCount + 1).toString() + "\",\"name\":\"" + catName + "\"}}}"
+    body: JSON.stringify(
+      {
+        "operation": "create",
+        "tableName": "CatTable",
+        "payload": {
+            "Item": {
+                "Id": `${ catCount + 1 }`,
+                "name": `${ catName }`
+            }
+        }
+      }
+    )
   })
   .then(
     (response) => {
@@ -19,7 +30,7 @@ addCat = (catName, catCount, catBack) => {
   )
   .catch(
     (err) => {
-      console.log("'Tis erroneous, Sire: ", err)
+      console.log("'Tis erroneous: ", err)
     }
   )
 };
